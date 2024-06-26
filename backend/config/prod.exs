547ef77365557c6e50cee85b4603af9a69c37dfe
rekkice <lekkice0@gmail.com>
2024-06-26 08:32:44 -0400

    cambios dockerfile

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -6,8 +6,8 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Backend.Finch
 # Disable Swoosh Local Memory Storage
 config :swoosh, local: false
 
-# Do not print debug messages in production
-config :logger, level: :info
+# # Do not print debug messages in production
+# config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
